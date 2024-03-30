f6a7052e2c839459c7bf243a5eb5a369719bd0f4
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2024-03-30 11:05:56 +0200

    Update darkThemeContext.js

--- context/darkThemeContext.js
+++ context/darkThemeContext.js
@@ -3,9 +3,16 @@ import { createContext, useState, useEffect } from "react";
 
 export const Theme = createContext();
 export const ThemeProvider = ({ children }) => {
-  const [mode, setMode] = useState(
-    () => localStorage.getItem("themeMode") || "light"
-  );
+  const [mode, setMode] = useState("light"); // default state
+
+  useEffect(() => {
+    // This code runs after the component has mounted, where `localStorage` is available
+    const savedMode = localStorage.getItem("themeMode");
+    if (savedMode) {
+      setMode(savedMode);
+    }
+  }, []);
+
   const toggle = () => {
     setMode((prev) => {
       const newMode = prev === "light" ? "dark" : "light";
@@ -13,6 +20,7 @@ export const ThemeProvider = ({ children }) => {
       return newMode;
     });
   };
+
   return (
     <Theme.Provider value={{ toggle, mode }}>
       <div className={`${mode}`}>{children}</div>
